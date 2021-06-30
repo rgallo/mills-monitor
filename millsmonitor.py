cf07e7da0f7d971bf594ec5ead12135518b41b67
Ryan Gallo <ryan.j.gallo@gmail.com>
2021-06-29 20:15:20 -0400

    Add minute mode, show real names of items

--- millsmonitor.py
+++ millsmonitor.py
@@ -15,6 +15,7 @@ def handleItem(name, itemlist, dataurl, funcs, pingrole=None):
     data = requests.get(dataurl).json()
     sorted_items = funcs["sorted_items"](data)
     count = funcs["count"](data)
+    names = funcs["names"]([funcs['id'](item) for item in sorted_items])
     s = (f"__**{name}:**__\nCurrent {name}: {count}, "
          f"Progress to Next: {funcs['to_next'](data)*100.0:.2f}%\n"
          f"Top {name}: ")
@@ -24,11 +25,11 @@ def handleItem(name, itemlist, dataurl, funcs, pingrole=None):
         if idx == count + 1:
             s += "\n----------"
         if idx <= len(itemlist) and item_id not in itemlist:
-            s += f"\n{idx}. {item_id} ({item_pct}%) {':question:' if all_wanted else ':x:'}"
+            s += f"\n{idx}. {names[item_id]} ({item_pct}%) {':question:' if all_wanted else ':x:'}"
             if pingrole and not all_wanted:
                 s += f" <@&{pingrole}>"
         else:
-            s += f"\n{idx}. {item_id} ({item_pct}%)"
+            s += f"\n{idx}. {names[item_id]} ({item_pct}%)"
             if item_id in itemlist:
                 s += " :white_check_mark:" if idx <= count else " :pray:"
     return s
@@ -40,7 +41,8 @@ def handleRenos(renolist, pingrole=None):
         "count": lambda data: data['progress']['total'],
         'to_next': lambda data: data['progress']['toNext'],
         "id": lambda item: item['id'],
-        "percent": lambda item: item['percent']
+        "percent": lambda item: item['percent'],
+        "names": lambda ids: {attr["id"]: attr["title"] for attr in requests.get(f"https://www.blaseball.com/database/renovations?ids={','.join(ids)}").json()}
     }
     return handleItem("Renovations", renolist, "https://www.blaseball.com/database/renovationProgress?id=ba794e99-4d6b-4e12-9450-6522745190f8", funcs, pingrole=pingrole)
 
@@ -51,7 +53,8 @@ def handleGifts(giftlist, pingrole=None):
         "count": lambda data: data["teamProgress"]["36569151-a2fb-43c1-9df7-2df512424c82"]['total'],
         'to_next': lambda data: data["teamProgress"]["36569151-a2fb-43c1-9df7-2df512424c82"]['toNext'],
         "id": lambda item: item['bonus'],
-        "percent": lambda item: f"{item['percent'] * 100.0:.2f}"
+        "percent": lambda item: f"{item['percent'] * 100.0:.2f}",
+        "names": lambda ids: {gift["id"]: gift["title"] for gift in requests.get("https://www.blaseball.com/database/offseasonSetup").json()["gifts"]}
     }
     return handleItem("Gifts", giftlist, "https://www.blaseball.com/database/giftProgress", funcs, pingrole=pingrole)
 
@@ -63,6 +66,7 @@ def handle_args():
     parser.add_argument('--renos', help="top reno list comma separated")
     parser.add_argument('--pingrole', help="id of role to ping")
     parser.add_argument('--pingday', help="when to start pinging", default="27")
+    parser.add_argument('--minutemode', help="don't run on 5s", action='store_true')
     args = parser.parse_args()
     return args
 
@@ -71,6 +75,8 @@ def main():
     if not 26 <= day <= 72:
         sys.exit()
     args = handle_args()
+    if args.minutemode and not datetime.datetime.now().minute % 5 and day < 71:
+        sys.exit()
     pingrole = args.pingrole if day > int(args.pingday) else None
     renos = args.renos.split(",") if args.renos else []
     gifts = args.gifts.split(",") if args.gifts else []
