165220b3b8300269795b660ffa10900e35276431
Ryan Gallo <ryan.j.gallo@gmail.com>
2021-07-01 10:11:01 -0400

    Add item actual spend totals to renos

--- millsmonitor.py
+++ millsmonitor.py
@@ -20,14 +20,20 @@ def handleItem(name, gooditems, baditems, dataurl, funcs, pingrole=None):
     sorted_items = funcs["sorted_items"](data)
     count = funcs["count"](data)
     names = funcs["names"]([funcs['id'](item) for item in sorted_items])
+    total_spent, remaining_to_next = funcs["total_spent"](data) if funcs.get("total_spent", None) else (None, None)
+    to_next = funcs['to_next'](data)*100.0
+    newline = "\n"
     s = (f"__**{name}:**__\nCurrent {name}: {count}, "
-         f"Progress to Next: {funcs['to_next'](data)*100.0:.2f}%\n"
+         f"Progress to Next: {to_next:.2f}%{f' ({remaining_to_next:,.0f})' if remaining_to_next else ''}\n"
+         f"{f'Total Spent: {total_spent:,.0f}{newline}' if total_spent else ''}"
          f"Top {name}: ")
     for idx, item in enumerate(sorted_items, start=1):
         item_id, item_pct = funcs['id'](item), funcs['percent'](item)
         if idx == count + 1:
             s += "\n----------"
-        s += f"\n{idx}. {names[item_id]} ({item_pct}%)"
+        s += f"\n{idx}. {names[item_id]} ({item_pct*100.0:.2f}%)"
+        if total_spent:
+            s += f" ({item_pct * total_spent:,.0f})"
         above_line = idx <= count
         is_good = item_id in gooditems
         is_bad = item_id in baditems
@@ -48,7 +54,15 @@ def handleItem(name, gooditems, baditems, dataurl, funcs, pingrole=None):
     return s
 
 
-def handleRenos(teamid, goodrenos, badrenos, pingrole=None):
+def get_total_spent_and_remaining(last_count, current_count, to_next):
+    total_achieved_spend = sum([(1000000*(1+0.5*(last_count-1)))*(3**i) for i in range(current_count)])
+    next_spend = sum([(1000000*(1+0.5*(last_count-1)))*(3**i) for i in range(current_count+1)])
+    partial_spend = (next_spend - total_achieved_spend) * to_next
+    total_spend = total_achieved_spend + partial_spend
+    return total_spend, next_spend - total_spend
+
+
+def handleRenos(teamid, goodrenos, badrenos, season, pingrole=None):
     stadiums = requests.get("https://api.sibr.dev/chronicler/v1/stadiums").json()["data"]
     stadium_id = None
     for stadium in stadiums:
@@ -57,13 +71,15 @@ def handleRenos(teamid, goodrenos, badrenos, pingrole=None):
             break
     if not stadium_id:
         raise Exception("invalid team id")
+    last_reno_count = len(requests.get(f"https://api.sibr.dev/eventually/v2/events?teamTags={teamid}&type=57&phase=5&season={season-2}").json())
     funcs = {
         "sorted_items": lambda data: sorted(data["stats"], key=lambda x: float(x["percent"]), reverse=True),
         "count": lambda data: data['progress']['total'],
         'to_next': lambda data: data['progress']['toNext'],
         "id": lambda item: item['id'],
-        "percent": lambda item: item['percent'],
-        "names": lambda ids: {attr["id"]: attr["title"] for attr in requests.get(f"https://www.blaseball.com/database/renovations?ids={','.join(ids)}").json()}
+        "percent": lambda item: float(item['percent']) / 100.0,
+        "names": lambda ids: {attr["id"]: attr["title"] for attr in requests.get(f"https://www.blaseball.com/database/renovations?ids={','.join(ids)}").json()},
+        "total_spent": lambda data: get_total_spent_and_remaining(last_reno_count, data['progress']['total'], data['progress']['toNext']),
     }
     return handleItem("Renovations", goodrenos, badrenos, f"https://www.blaseball.com/database/renovationProgress?id={stadium_id}", funcs, pingrole=pingrole)
 
@@ -74,7 +90,7 @@ def handleGifts(teamid, goodgifts, badgifts, pingrole=None):
         "count": lambda data: data["teamProgress"][teamid]['total'],
         'to_next': lambda data: data["teamProgress"][teamid]['toNext'],
         "id": lambda item: item['bonus'],
-        "percent": lambda item: f"{item['percent'] * 100.0:.2f}",
+        "percent": lambda item: item['percent'],
         "names": lambda ids: {gift["id"]: gift["title"] for gift in requests.get("https://www.blaseball.com/database/offseasonSetup").json()["gifts"]}
     }
     return handleItem("Gifts", goodgifts, badgifts, "https://www.blaseball.com/database/giftProgress", funcs, pingrole=pingrole)
@@ -99,11 +115,12 @@ def handle_args():
     return args
 
 def main():
-    day = requests.get("https://www.blaseball.com/database/simulationData").json()["day"]
-    if not 26 <= day <= 72:
+    simdata = requests.get("https://www.blaseball.com/database/simulationData").json()
+    season, day = simdata["season"] + 1, simdata["day"] + 1
+    if not 27 <= day <= 72:
         sys.exit()
     args = handle_args()
-    if args.minutemode and not datetime.datetime.now().minute % 5 and day < 71:
+    if args.minutemode and not datetime.datetime.now().minute % 5 and day <= 71:
         sys.exit()
     pingrole = args.pingrole if day > int(args.pingday) else None
     if args.configurl:
@@ -119,7 +136,7 @@ def main():
         badgifts = args.badgifts.split(",") if args.badgifts else []
     sep = '-' * 20
     outputstr = f"{sep}**{datetime.datetime.now().strftime('%I:%M %p')}**{sep}\n"
-    outputstr += handleRenos(args.teamid, goodrenos, badrenos, pingrole)
+    outputstr += handleRenos(args.teamid, goodrenos, badrenos, season, pingrole)
     outputstr += "\n\n"
     outputstr += handleGifts(args.teamid, goodgifts, badgifts, pingrole)
     output(args.webhook, outputstr, discord=not args.print)
